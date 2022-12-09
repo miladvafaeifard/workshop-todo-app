46df5da45d1e219d1ba19bb742c9137879c595d8
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2022-12-09 16:55:05 +0100

    Enhance add and delet functions

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ console.clear();
 
 const [addBtn] = document.getElementsByClassName('add-btn');
 const [inputText] = document.getElementsByClassName('text-input');
+const [tasksList] = document.getElementsByClassName('tasks-list');
 
-addBtn.addEventListener('click', $event => {
-    inputText.value = '';
+addBtn.addEventListener('click', ($event) => {
+  createNewTask();
 });
+
+function createNewTask() {
+  const li = document.createElement('li');
+  const checkBoxInput = document.createElement('input');
+  const textNode = document.createTextNode(inputText.value);
+  checkBoxInput.setAttribute('type', 'checkbox');
+  li.appendChild(checkBoxInput);
+  li.appendChild(textNode);
+  li.setAttribute('class', 'task-item');
+  li.onclick = deleteTask;
+  tasksList.append(li);
+  inputText.value = '';
+}
+
+function deleteTask($event) {
+  $event.target.remove();
+}
