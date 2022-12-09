579ec691d9945c3e3f6d59daf9c29f49f9b3703c
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2022-12-09 15:40:49 +0100

    Add input and button

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+console.clear();
+
+const [addBtn] = document.getElementsByClassName('add-btn');
+const [inputText] = document.getElementsByClassName('text-input');
+
+addBtn.addEventListener('click', $event => {
+    inputText.value = '';
+});
